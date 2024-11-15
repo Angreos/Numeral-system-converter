412de3578e03130a5c86ff468eaed004ebe0fe72
Angreos <schmidtandreas102@gmail.com>
2024-11-15 01:04:20 +0100

    Added the ability to convert from the base 10 into base n
    Can convert from base 10 into any base now.
    
    Also added some Colors.

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,15 @@
+import NumeralSystemConverter.Colors;
 import NumeralSystemConverter.ConverterApp;
 import NumeralSystemConverter.Converter;
+import NumeralSystemConverter.Conversion;
 
 public class Main {
     public static void main(String[] args) {
-        // Description
-        System.out.println("""
-                This application was made by Andreas.
-                https://github.com/Angreos
-                
-                With this you can convert numbers from one numeral system to different one.
-                For example, you can convert 1110 from binary to base 10 which would give you 14.
-                Works with decimal numbers.
-                """);
+        // Description and github link
+        System.out.println(Colors.GREEN_BOLD +"This application was made by Andreas."+Colors.RESET+"\nhttps://github.com/Angreos");
+        System.out.println("Convert numbers from one numeral system to a different one, with or without steps.");
 
         // starts the Application
-
         ConverterApp.run();
-
-/*
-        String number = "F.1";
-        String numberConverted = Converter.convertToBase10(number, 16, true);
-        System.out.println(numberConverted);
-        */
-
     }
 }
